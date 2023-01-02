f9ccbe249369396f003ca5cb4a4a871ea86006fe
hchend2 <hchend2@uic.edu>
2023-01-02 17:01:58 -0600

    check

--- parity.py
+++ parity.py
@@ -7,7 +7,8 @@ def main():
     
 
 def is_even(n):
-    return True if n%2 == 0 else False
+    return n%2 == 0
+    #return True if n%2 == 0 else False
     #if (n%2 == 0):
     #    return True
     #else:
