9656ca92f439d4f1170f650c88a7fdab0bb5065b
hchend2 <hchend2@uic.edu>
2023-01-02 16:52:04 -0600

    improved

--- parity.py
+++ parity.py
@@ -1,6 +1,16 @@
-x = int (input("What is x?"))
+def main():
+    x = int (input("What is x? "))
+    if (is_even(x)):
+        print ("X is EVEN")
+    else:
+        print ("X is ODD")
+    
 
-if (x % 2 == 0):
-    print ("X is EVEN")
-else:
-    print ("X is ODD")
+def is_even(n):
+    if (n%2 == 0):
+        return True
+    else:
+        return False
+
+if (__name__ == "__main__"):
+    main()
